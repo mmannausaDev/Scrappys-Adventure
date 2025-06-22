e167f8e658b605a39e883b0b481ade0328f17282
AndrewR115 <sk8erpro@live.com>
2025-06-22 15:56:08 -0500

    Created asteroid minigame that is to be played in the cafe. Score system to be implemented later

---
+++ Assets/Scripts/ShipMinigame/PlaterHealth.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaterHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    [SerializeField] AudioSource hitsound;
+    public Healthbar healthbar;
+    public GameObject obj;
+    SpriteRenderer sprite;
+    bool canTakeDamage = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(PlayerPrefs.GetInt("Health") > 100)
+        {
+            maxHealth = PlayerPrefs.GetInt("Health");
+        }
+
+        currentHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
+        obj = GameObject.FindGameObjectWithTag("GM");
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            Destroy(this.gameObject);        
+        }
+    }
+
+    public void takeDamage(int dmg)
+    {
+        currentHealth -= dmg;
+        hitsound.Play();
+
+        healthbar.SetHealth(currentHealth);
+
+        StartCoroutine(HitBlink());
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            if (canTakeDamage)
+            {
+                takeDamage(1);
+            }
+        }
+    }
+
+    IEnumerator HitBlink()
+    {
+        canTakeDamage = false;
+        int count = 0;
+        while (count < 3)
+        {
+            sprite.enabled = false;
+            yield return new WaitForSeconds(0.25f);
+            sprite.enabled = true;
+            yield return new WaitForSeconds(0.25f);
+            count++;
+        }
+        canTakeDamage = true;
+    }
+}
