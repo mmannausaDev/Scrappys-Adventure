43c096f238ff21889a012d34b3c52b3beb05db37
mmannausaDev <164946543+mmannausaDev@users.noreply.github.com>
2025-06-21 21:33:35 -0400

    working on fishing game

--- Assets/Scripts/ScrapGame.cs
+++ Assets/Scripts/ScrapGame.cs
@@ -1,15 +1,31 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ScrapGame : MonoBehaviour
 {
 
-    [SerializeField] Transform hook;
+    [SerializeField] Rigidbody2D hook;
+    [SerializeField] int hookSpeed;
+
+    //might not be needed 
     [SerializeField] private Vector2 minGameBoundry;
     [SerializeField] private Vector2 maxGameBoundry;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] GameObject lastSpawnedScreen; 
+    [SerializeField] private Transform spawnLocation;
+    [SerializeField] private List<GameObject> gameObjectsToSpawn = new List<GameObject>();
+    [SerializeField] private GameObject nextScreen;
+
+    private void Start()
     {
-        
+        SpawnObject();
+        SpawnObject();
+        SpawnObject();
+        SpawnObject();
+
+        SpawnScreen(); 
     }
 
     // Update is called once per frame
@@ -17,15 +33,34 @@ public class ScrapGame : MonoBehaviour
     {
 
         Vector3 screenPoint = Input.mousePosition;
-        Debug.Log(screenPoint); 
         Vector3 tempHookPos = Camera.main.ScreenToWorldPoint(screenPoint);
-        Debug.Log(tempHookPos);
-        tempHookPos.x = Mathf.Clamp(tempHookPos.x, minGameBoundry.x, maxGameBoundry.x);
-        tempHookPos.y = Mathf.Clamp(tempHookPos.y, minGameBoundry.y, maxGameBoundry.y);
-        tempHookPos.z = 10.0f;
-        Debug.Log(tempHookPos);
-        hook.position = tempHookPos; 
+        //tempHookPos.x = Mathf.Clamp(tempHookPos.x, minGameBoundry.x, maxGameBoundry.x);
+        //tempHookPos.y = Mathf.Clamp(tempHookPos.y, minGameBoundry.y, maxGameBoundry.y);
+        //tempHookPos.z = 10.0f;
+
+        //hook.position = Vector3.MoveTowards(hook.position, tempHookPos,  hookSpeed * Time.deltaTime);
+        hook.MovePosition(hook.position + new Vector2(tempHookPos.x - hook.position.x, tempHookPos.y - hook.position.y) * hookSpeed * Time.deltaTime);
+        //hook.position = tempHookPos; 
+        Debug.Log(lastSpawnedScreen.transform.position.y); 
+        if(lastSpawnedScreen.transform.position.y >= 0)
+        {
+            SpawnScreen(); 
+        }
 
 
     }
+
+    private void SpawnScreen()
+    {
+        lastSpawnedScreen = Instantiate(nextScreen, new Vector3( spawnLocation.position.x, spawnLocation.position.y, 9), Quaternion.identity, gameObject.transform);
+    }
+
+    private void SpawnObject()
+    {
+        Instantiate(gameObjectsToSpawn[0], 
+            new Vector2( spawnLocation.position.x + Random.Range(-3, 3), spawnLocation.position.y + Random.Range(-5, 5)), 
+            Quaternion.identity);
+    }
+
+
 }
---
+++ Assets/Scripts/ScrapGameObj.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class ScrapGameObj : MonoBehaviour
+{
+
+    [SerializeField] private int currentSpeed = 2;
+    private int fastSpeed = 8; 
+    private int slowSpeed = 2;
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (UnityEngine.Input.GetMouseButtonDown(0))
+        {
+            currentSpeed = fastSpeed;
+            Debug.Log("going faster"); 
+
+        }
+        if (UnityEngine.Input.GetMouseButtonUp(0))
+        {
+            currentSpeed = slowSpeed;
+        }
+        transform.Translate(Vector3.up * currentSpeed * Time.deltaTime);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("picked up item"); 
+        //do something with the item picked up
+        gameObject.SetActive(false);
+
+    }
+}
